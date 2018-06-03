b9c023b9d24d548677f61c4552747aff94c5feda
KirillYaykov <anton97rus@gmail.com>
2018-06-03 20:14:31 +0400

    final

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(452, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Решить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 51);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(544, 335);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(470, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Обзор";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(422, 392);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Сохранить как";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 427);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string[] ways = new string[4] { "S", "E", "N", "W" };
+        int[] arrayHW = new int[6];
+        List<string> prohod = new List<string>();
+        List<string> prohodOut = new List<string>();
+        string cur_or = "S";
+        string[,] arrayLab = new string[1000, 1000];
+        int cur_x = 0;
+        int cur_y = 0;
+        int max;
+        string filePathOpen = "";
+        OpenFileDialog ofd = new OpenFileDialog();
+        SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+
+        public void MakeTable()
+        {
+            prohod.Add("1000"); prohod.Add("0100"); prohod.Add("1100"); prohod.Add("0010");
+            prohod.Add("1010"); prohod.Add("0110"); prohod.Add("1110"); prohod.Add("0001");
+            prohod.Add("1001"); prohod.Add("0101"); prohod.Add("1101"); prohod.Add("0011");
+            prohod.Add("1011"); prohod.Add("0111"); prohod.Add("1111");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            MakeTable();
+            prohodOut = prohod;
+        }
+
+        public string FileTxt() //считывание
+        {
+            string line;            
+            System.IO.StreamReader file = new System.IO.StreamReader(filePathOpen);
+            string inText = "";
+            while ((line = file.ReadLine()) != null)
+            {
+                inText += line + "\n"; 
+                //textBox2.Text += line + "\r\n";                
+            }
+
+            file.Close();
+            return inText;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            string inText = FileTxt();
+            int strCount = int.Parse(inText.Split('\n')[0].ToString());
+            string subStr1, subStr2;
+            string strTest = ""; int temp = 0; 
+           string outStr = "";
+            for (int i = 1; i <= strCount; i++)
+            {
+                subStr1 = inText.Split('\n')[i].ToString().Split(' ')[0].ToString();
+                subStr2 = inText.Split('\n')[i].ToString().Split(' ')[1].ToString();
+                max = Math.Max(subStr1.Length, subStr2.Length);
+                max=max+4;
+                cur_or = "S";
+                cur_x = max/2;
+                cur_y = max-1;
+                for (int j = 0; j < max; j++)
+                {
+                    for (int k = 0; k < max; k++)
+                    {
+                        arrayLab[j, k] = "xxxx";
+                    }
+                }
+                outStr = "Case #" + i +":"+ "\r\n";
+                textBox2.Text += outStr;
+                lab_norm(subStr1);
+                if(cur_or=="N")
+                {
+                    cur_or = "S";
+                    cur_y--;
+                }
+                else if(cur_or=="S")
+                {
+                    cur_or = "N";
+                    cur_y++;
+                }
+                else if (cur_or == "E")
+                {
+                    cur_or = "W";
+                    cur_x--;
+                }
+                else if (cur_or == "W")
+                {
+                    cur_or = "E";
+                    cur_x++;
+                }
+                lab_norm(subStr2);
+                Output();
+            }
+        }
+
+        private string lab_norm (string str_in)
+        {
+           for(int i=1;i<str_in.Length;i++)
+            {
+                string sub = str_in.Substring(i, 1);
+                if (cur_or == "S")
+                {
+                    if (sub == "R")
+                    {
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 3, '0');
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 1, '0');
+                        cur_or = "W";
+                    }
+                    else if (sub == "W")
+                    {
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 1, '1');
+                        if (str_in.Substring(i - 1, 1) != "L")
+                        {
+                            arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 3, '0');
+                        }
+                        cur_y = cur_y - 1;
+                    }
+                    else if (sub == "L")
+                    {
+                        cur_or = "E";
+                    }
+                }
+                else if (cur_or == "W")
+                {
+                    if (sub == "R")
+                    {
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 1, '0');
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 2, '0');
+                        cur_or = "N";
+                    }
+                    else if (sub == "W")
+                    {
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 2, '1');
+                        if (str_in.Substring(i - 1, 1) != "L")
+                        {
+                            arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 1, '0');
+                        }
+                        cur_x = cur_x - 1;
+                    }
+                    else if (sub == "L")
+                    {
+                        cur_or = "S";
+                    }
+                }
+                else if (cur_or == "N")
+                {
+                    if (sub == "R")
+                    {
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 2, '0');
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 0, '0');
+                        cur_or = "E";
+                    }
+                    else if (sub == "W")
+                    {
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 0, '1');
+                        if (str_in.Substring(i - 1, 1) != "L")
+                        {
+                            arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 2, '0');
+                        }
+                        cur_y = cur_y + 1;
+                    }
+                    else if (sub == "L")
+                    {
+                        cur_or = "W";
+                    }
+                }
+                else if (cur_or == "E")
+                {
+                    if (sub == "R")
+                    {
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 0, '0');
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 3, '0');
+                        cur_or = "S";
+                    }
+                    else if (sub == "W")
+                    {
+                        arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 3, '1');
+                        if (str_in.Substring(i - 1, 1) != "L")
+                        {
+                            arrayLab[cur_x, cur_y] = ReplaceCharInString3(arrayLab[cur_x, cur_y], 0, '0');
+                        }
+                        cur_x = cur_x + 1;
+                    }
+                    else if (sub == "L")
+                    {
+                        cur_or = "N";
+                    }
+                }
+            }
+            return "0";
+        }
+        
+        public string ReplaceCharInString3(string source, int index, Char newSymb)
+        {
+            char[] chars = source.ToCharArray();
+            chars[index] = newSymb;
+            string tt = new string(chars);
+            return tt;
+        }
+
+
+
+        private void Output ()
+        {
+            int start_x=0, start_y=0, end_x=0, end_y=0;
+                for (int k = 0; k < max; k++)
+                {
+                    if(arrayLab[k, max-1] != "xxxx")
+                    {
+                        start_x = k;
+                    break;
+                    }
+                }
+            for (int k = max-1; k >= 0; k--)
+            {
+                if (arrayLab[k, max-1] != "xxxx")
+                {
+                    end_x = k;
+                    break;
+                }
+            }
+            for (int k = 0; k < max; k++)
+            {
+                if (arrayLab[max/2, k] != "xxxx")
+                {
+                    end_y = k;
+                    break;
+                }
+            }
+            for (int k = max-1; k >= 0; k--)
+            {
+                if (arrayLab[max/2, k] != "xxxx")
+                {
+                    start_y = k;
+                    break;
+                }
+            }
+            for (int i=start_y;i>=end_y;i--)
+            {
+                for (int j = start_x; j <= end_x; j++)
+                {
+                    arrayLab[j, i]=exchange(arrayLab[j, i]);
+                    textBox2.Text += arrayLab[j, i]+" ";
+                }
+                textBox2.Text += "\r\n";
+            }
+        }
+
+        private string exchange(string inp)
+        {
+            for(int i=0;i<15;i++)
+            {
+                if(inp==prohod[i])
+                {
+                    if(i<=8)
+                        inp=(i+1).ToString();
+                    else
+                    {
+                        if (i == 9)
+                            inp = "a";
+                        else if (i == 10)
+                            inp = "b";
+                        else if (i == 11)
+                            inp = "c";
+                        else if (i == 12)
+                            inp = "d";
+                        else if (i == 13)
+                            inp = "e";
+                        else if (i == 14)
+                            inp = "f";
+                    }
+                }
+            }
+            return inp;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filePathOpen = ofd.FileName;
+                textBox1.Text = filePathOpen;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(filename, textBox2.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+    }
+}
